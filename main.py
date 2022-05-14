73ad86ba69ffad519b881a808758e7a53d68de17
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2022-05-14 21:43:16 +0700

    update main function

--- main.py
+++ main.py
@@ -3,30 +3,35 @@ from match import Match
 from table import Table
 from helper import export_to_txt
 
-import os
+
+def main():
+    # Define input file
+    input_file = "sample_input.txt"
+
+    # Data extraction
+    de = DataExtraction()
+    de.setFile(input_file)
+    data = de.getData()
+
+    # Objects initialisation
+    match = Match()
+    table = Table()
+
+    # Update entries to table
+    for entry in data:
+        t1_info, t2_info = entry
+        match.update_match(t1_info, t2_info)
+        match_result = match.analyze()
+        table.addEntry(match_result)
+
+    # Sort and assign teams' ranks to table
+    table.sort_table()
+    table.rank_table()
+
+    # Export table to text file
+    table_dict = table.getTableDict()
+    export_to_txt(table_dict)
 
 
-# ======================================================================================================================
-input_file = "sample_input.txt"
-de = DataExtraction()
-de.setFile(input_file)
-data = de.getData()
-
-match = Match()
-table = Table()
-
-for entry in data:
-    t1_info, t2_info = entry
-    match.update_match(t1_info, t2_info)
-    match_result = match.analyze()
-    table.addEntry(match_result)
-
-table.sort_table()
-table.rank_table()
-table_dict = table.getTableDict()
-
-for item in table_dict.items():
-    print(item)
-
-export_to_txt(table_dict)
-
+if __name__ == '__main__':
+    main()
